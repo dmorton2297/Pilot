ef768b2c728f12e04ff24459c1fc2f1b7a910a42
juddmic <juddmic@gmail.com>
2018-09-23 19:06:21 -0400

    generate scaffolding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ScrumBoardComponent } from './scrum-board/scrum-board.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ScrumBoardComponent
   ],
   imports: [
     BrowserModule
