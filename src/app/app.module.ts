0a3b59e152e9e373fc0d863c0f695cb41d5d9a27
Dan Morton <dmorton2297@gmail.com>
2018-09-26 09:25:38 -0400

    Fixed more dependency issues

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,14 +10,16 @@ import { RouterModule, Routes } from '@angular/router';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {NavigationComponent} from './navigation/navigation.component';
 import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+
+
 
 const appRoutes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'scrum', component: ScrumBoardComponent }
 ];
 
-import { ScrumBoardComponent } from './scrum-board/scrum-board.component';
 
 import { CreateTaskComponent } from './create-task/create-task.component';
 
@@ -25,18 +27,15 @@ import { CreateTaskComponent } from './create-task/create-task.component';
   declarations: [
     AppComponent,
     LoginComponent,
-<<<<<<< HEAD
-    NavigationComponent,
-    ScrumBoardComponent
-=======
     CreateTaskComponent,
     NavigationComponent
->>>>>>> feat/createtask
   ],
   imports: [
   BrowserModule,
   MatCardModule,
   MatButtonModule,
+  MatSelectModule,
+  MatFormFieldModule,
   MatToolbarModule,
   BrowserAnimationsModule,
   RouterModule.forRoot(
