bed99091f1737090259dc7d80f6827cbe937c39d
Dan Morton <dmorton2297@gmail.com>
2018-10-01 15:11:07 -0400

    Added create task button to backlog view

--- src/app/backlog/backlog.component.ts
+++ src/app/backlog/backlog.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Http } from '@angular/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-backlog',
@@ -11,10 +12,14 @@ export class BacklogComponent {
   public tasks: Task[] = [];
   public displayedColumns: String[] = ['id', 'name', 'description', 'priority', 'status', 'created'];
 
-  constructor(private http: Http) { 
+  constructor(private http: Http, private router: Router) { 
     this.http.get('http://localhost:8000/api/getusertasks/0').subscribe((res) => {
       this.tasks = res.json() as Task[];
     });
+  }
+
+  onCreateTask() {
+    this.router.navigateByUrl('/createtask');
   }
 
 }
