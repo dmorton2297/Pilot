f850a926d5dda4f3abc7b78cdf3c3fd430f6851a
Dan Morton <dmorton2297@gmail.com>
2018-10-03 15:14:07 -0400

    Added change status functionality to backend view

--- src/app/backlog/backlog.component.ts
+++ src/app/backlog/backlog.component.ts
@@ -13,13 +13,40 @@ export class BacklogComponent {
   public displayedColumns: String[] = ['id', 'name', 'description', 'priority', 'status', 'created'];
 
   constructor(private http: Http, private router: Router) { 
+    this.loadData();
+  }
+
+  onCreateTask() {
+    this.router.navigateByUrl('/createtask');
+  }
+
+  loadData() {
     this.http.get('http://localhost:8000/api/getusertasks/0').subscribe((res) => {
       this.tasks = res.json() as Task[];
     });
   }
 
-  onCreateTask() {
-    this.router.navigateByUrl('/createtask');
+  onStatusClicked(taskId: number) {
+    var newStatus = 0;
+    for (var i = 0; i < this.tasks.length; i++) {
+      if (this.tasks[i].id == taskId) {
+        if (this.tasks[i].status == 0) {
+          newStatus = 1;
+        } else if (this.tasks[i].status == 1){
+          newStatus = 2;
+        } else {
+          newStatus = 0;
+        }
+      }
+    }
+    let request : ChangeStatusRequest = {
+      taskId : taskId,
+      status: newStatus
+    }
+
+    this.http.post('http://localhost:8000/api/changestatus', request).subscribe((res) => {
+      this.loadData();
+    });
   }
 
 }
@@ -38,4 +65,9 @@ interface Task {
   creatorid: number,
   assigneduserid: number,
   created_at: number;
+}
+
+interface ChangeStatusRequest {
+    taskId: number,
+    status: number
 }
