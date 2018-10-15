7ea241afac88dbeefbf447c948118df3ad0d505a
juddmic <juddmic@gmail.com>
2018-10-14 22:27:25 -0400

    create team scaffolding

---
+++ server/app/Http/Controllers/TeamController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class TeamController extends Controller
+{
+	//Displays all members of a team
+    public function index(Request $request) {
+		
+	}
+	
+	//Adds user to team
+	public function add(Request $request) {
+		
+	}
+	
+	//Removes user from team
+	public function kick(Request $request) {
+	}
+}
