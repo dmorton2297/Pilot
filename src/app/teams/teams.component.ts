f746d8ba4dbb732c0d9135119a17d0291aaf2ac3
Dan Morton <dmorton2297@gmail.com>
2018-10-21 18:59:32 -0400

    Added view to see all teams created by user

--- src/app/teams/teams.component.ts
+++ src/app/teams/teams.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Http } from '@angular/http';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-teams',
@@ -7,8 +9,14 @@ import { Router } from '@angular/router';
   styleUrls: ['./teams.component.css']
 })
 export class TeamsComponent implements OnInit {
+  public teams: Team[];
+  public displayedColumns: String[] = ['id', 'name', 'description', 'created'];
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private http: Http, private auth: AuthService) { 
+    this.http.get('http://localhost:8000/api/getusersteams/' + this.auth.getUserId()).subscribe((res) => {
+      this.teams = res.json() as Team[];
+    });
+  }
 
   ngOnInit() {
   }
@@ -17,4 +25,12 @@ export class TeamsComponent implements OnInit {
     this.router.navigateByUrl('/createteam');
   }
 
+}
+
+interface Team {
+  name: string,
+  description: string,
+  invitemsg: string,
+  color: any
+  creatorId: number
 }
