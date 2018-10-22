1acc1b26573068c574d3901517d13dda8f430d1d
Dan Morton <dmorton2297@gmail.com>
2018-10-21 20:36:10 -0400

    View teams and view team detail views

--- src/app/view-team/view-team.component.ts
+++ src/app/view-team/view-team.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-view-team',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewTeamComponent implements OnInit {
 
-  constructor() { }
+  teamId: string;
+  team: Team;
+  public teamName: string = "";
+  constructor(private activatedRoute: ActivatedRoute, private router: Router, private http: Http) { 
+    this.teamId = this.activatedRoute.snapshot.paramMap.get('id');
+    this.http.get('http://localhost:8000/api/getteam/' + this.teamId).subscribe((res) => {
+      console.log(res.json);
+      var temp = res.json() as Team[];
+      this.team = temp[0];
+      this.teamName = this.team.name;
+      console.log(this.teamName);
+    });
+  }
 
   ngOnInit() {
   }
 
 }
+
+interface Team {
+  id: number,
+  name: string,
+  description: string,
+  invitemsg: string,
+  color: any
+  creatorId: number,
+  created_at: number,
+  updated_at: number
+}
+
+
