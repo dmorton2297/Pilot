340ee33bc2eeedb227c822a3b279605690d35718
Dan Morton <dmorton2297@gmail.com>
2018-10-29 04:21:26 -0400

    get team members end point impemented in backend

--- server/routes/api.php
+++ server/routes/api.php
@@ -46,7 +46,8 @@ Route::get('teaminvites/{userid}', 'TeamInvitationController@index');
 Route::get('deleteinvite/{id}', 'TeamInvitationController@remove');
 Route::post('inviteuser', 'TeamInvitationController@invite');
 Route::get('allsentinvites/{id}', 'TeamInvitationController@detailedUserIndex');
-Route::Get('allrecievedinvites/{id}', 'TeamInvitationController@getUserInvitations');
+Route::get('allrecievedinvites/{id}', 'TeamInvitationController@getUserInvitations');
+Route::get('getteammembers/{teamId}', 'TeamAssignmentController@getTeamMembers');
 
 
 
